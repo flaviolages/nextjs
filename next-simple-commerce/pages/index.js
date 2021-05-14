c4b145a254a0f458b62c388c8dd8c56c576e04bc
flavio lages <meflaviolages@gmail.com>
2021-05-14 19:38:47 -0300

    ico

--- next-simple-commerce/pages/index.js
+++ next-simple-commerce/pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clean Next.js - Tailwindcss</title>
+        <title>Jet Company Br - commerce</title>
         <meta name="description" content="Clean" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://res.cloudinary.com/djhntsyxr/image/upload/v1619115322/jetcompanybr/favicon-jetcompanybr_wnwgvy.png" />
       </Head>
 
       <Layout>
