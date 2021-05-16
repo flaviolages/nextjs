c7c6f83b1ca150da2ce10d53f5a622cb4c79abba
flavio lages <meflaviolages@gmail.com>
2021-05-16 15:38:30 -0300

    menu

--- next-simple-commerce/components/MainContent.js
+++ next-simple-commerce/components/MainContent.js
@@ -1,4 +1,5 @@
 import { MdAddShoppingCart } from 'react-icons/md'
+import { AiFillCaretDown } from 'react-icons/ai'
 
 function MainContent({produtos}) {
     return(
@@ -6,7 +7,15 @@ function MainContent({produtos}) {
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 mt-3 mb-20">
 
                 <div className="col-span-8 lg:col-span-2 order-1 lg:order-none">
-                <div class="lg:hidden"><span class="rounded-md shadow-sm"><button type="button" class="flex justify-between w-full rounded-sm border border-gray-300 px-4 py-3 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" aria-expanded="true">All Categories<svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button></span></div>
+                
+                <div class="lg:hidden"><span class="rounded-md shadow-sm">
+                    <button type="button" class="flex justify-between w-full rounded-sm border border-gray-300 px-4 py-3 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                        All Categories
+                        <span className="text-xs">
+                        <AiFillCaretDown />
+                        </span>
+                </button></span></div>
+                
                     <div className="relative inline-block w-full">
                         <div className="origin-top-left absolute lg:relative left-0 mt-2 w-full rounded-md shadow-lg lg:shadow-none z-10 mb-10 lg:block hidden">
                             <div className="rounded-sm bg-white shadow-xs lg:bg-none lg:shadow-none">
